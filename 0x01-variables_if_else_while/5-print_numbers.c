e15ae38b641dd4b20d12369570e5e5809c9fb91f
Sofiag8 <1943@holbertonschool.com>
2020-06-11 22:55:23 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 0;
+
+while (a < 10)
+printf("%d", a++);
+
+printf("\n");
+return (0);
+}
