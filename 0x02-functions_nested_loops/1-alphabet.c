a670b00bd08d7bb71cffa9b5437afde1e0eb446b
Sofiag8 <1943@holbertonschool.com>
2020-06-16 01:00:01 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ *
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+	_putchar(a);
+	a++;
+}
+	_putchar('\n');
+
+}
