46d90901e78c7baf2187edde198366c1fe0f0a0e
Sofiag8 <1943@holbertonschool.com>
2020-06-18 17:59:10 +0000

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: character to multiplie
+ * @b: character to multiplie
+ * Return: 0 is success
+ */
+int mul(int a, int b)
+{
+	int multiplication = a * b;
+
+	return (multiplication);
+}
