0f6ec5ff37bca7f308b74146ae9e342c613f3206
Sofiag8 <1943@holbertonschool.com>
2020-06-23 16:14:30 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,6 @@ void puts2(char *str)
 	int string;
 
 	for (string = 0; str[string] != '\0'; string += 2)
-		_putchar(str[string]);
 	if (string % 2 == 0)
 		_putchar(str[string]);
 	_putchar('\n');
