24cb4ce0bcfe0b7c9b1f2a2a39987e8317d61df0
Sofiag8 <1943@holbertonschool.com>
2020-06-23 15:56:55 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string += 2)
+		_putchar(str[string]);
+	_putchar('\n');
+}
