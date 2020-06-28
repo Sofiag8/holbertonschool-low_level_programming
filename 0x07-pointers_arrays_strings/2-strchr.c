5db4f13dde79b5bf5e82264eb6e8035536e46e8a
Sofiag8 <1943@holbertonschool.com>
2020-06-28 17:02:15 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: char to check
+ * @c: char to check
+ *
+ * Return: s if is found and null if is not
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int x;
+
+	for (;; x++)
+	{
+		if (s[x] == c)
+			return (&(s[x]));
+	}
+}
