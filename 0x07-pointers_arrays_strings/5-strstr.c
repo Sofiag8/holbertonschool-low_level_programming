ddf99dc35a4e252fc8b8b21222e1bf5f7ddf0b8c
Sofiag8 <1943@holbertonschool.com>
2020-06-29 21:19:01 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strstr - ffind the first occurrence
+ *
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: a pointer to needle or NULL if no byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a] == haystack[b])
+				return (&(needle[b]));
+		}
+	}
+	return (0);
+}
