2770d1dfa4176cc6876bdc92650c153c493cf02e
Sofiag8 <1943@holbertonschool.com>
2020-06-29 23:49:28 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != 0)
 	{
-		while (*haystack == *b && *b != 0)
+		while (*haystack == *b && *b != 0 && *haystack != 0)
 			haystack++, b++;
 		if (*b == 0)
 			return (a);
