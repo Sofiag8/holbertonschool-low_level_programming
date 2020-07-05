607ae09d5121c7ff46b4720f15eebd744ee0fc8a
Sofiag8 <1943@holbertonschool.com>
2020-07-05 06:05:28 +0000

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * main - program that prints its name
+ *
+ * @argc: the variable for the number or arguments
+ * @argv: an array varible containing the program arguments
+ * Return: 0 is success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, multiply;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	multiply = a * b;
+	{
+		printf("%d\n", multiply);
+		return (0);
+	}
+}
