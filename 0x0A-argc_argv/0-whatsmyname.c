6605972ece690e7e31a932cfe400fd3f481089ed
Sofiag8 <1943@holbertonschool.com>
2020-07-05 03:00:01 +0000

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - program that prints its name
  *
- * @argc: int variable to check
- *
+ * @argc: the variable for the number or arguments
+ * @argv: an array varible containing the program arguments
  * Return: 0 is success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
