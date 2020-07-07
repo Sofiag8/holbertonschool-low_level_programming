e78474c2714856221fd93f1316c79f542cf76903
Sofiag8 <1943@holbertonschool.com>
2020-07-07 20:53:53 +0000

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,15 +14,10 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	/* recorro con size str para saber su tamaño */
 	for (size = 0; str[size] != '\0'; size++)
-		/* agrego un espacio de memoria para el valor de ptr */
 		ptr = malloc((size + 1) * sizeof(char));
-	/* size + 1 así me agregua en la memoria espacio tambien para '\0' */
-	/* condición que me compruebe que el espacio de memoria fue agregado */
 	if (ptr == NULL)
 		return (NULL);
-	/*utilizo de nuevo size para agregar el contenido de str en ptr */
 	for (size = 0; str[size] != '\0'; size++)
 		ptr[size] = str[size];
 	return (ptr);
