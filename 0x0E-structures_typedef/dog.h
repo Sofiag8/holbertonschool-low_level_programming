a23edc9030858640a78ca7aafa03956bef8ba5f9
Sofiag8 <1943@holbertonschool.com>
2020-07-13 21:56:58 +0000

    library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,9 @@ typedef struct dog
 	char *owner;
 }
 	dog;
+
+typedef struct dog dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
