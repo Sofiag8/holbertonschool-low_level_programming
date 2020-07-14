b9054bae65488d31fe49bc2b3508844790708497
Sofiag8 <1943@holbertonschool.com>
2020-07-14 00:56:30 +0000

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,6 +11,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/* to access elements of a pointer to a structure */
+	/* you have to dereference the pointer */
+	/* and then access to the data */
+	/* the simple way to do this is using '->' symbol */
+	/* is the same than using '.' symbol */
+	/* e.g (*ptr).name is the same than ptr->name but easier :) */
+
 	if (d)
 	{
 		d->name = name;
