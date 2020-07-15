1099cc72411ce4afc15d9afab17738cba6d91a60
Sofiag8 <1943@holbertonschool.com>
2020-07-15 18:16:07 +0000

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	/* we already know the size of the array */
 	/* elements runs while it is less than the size ofc. */
 	for (elements = 0; elements < size; elements++)
-	{
 		action(array[elements]);
-	}
 	/* so de-reference the pointer to the function */
 	/* to the elements of the array */
 }
