1b53d8181b4a72f27f743dfdcd93ca69180c450e
Sofiag8 <1943@holbertonschool.com>
2020-07-15 18:12:14 +0000

    My Library

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
-
-
+#include <stddef.h>
+/* stddef.h defines various variable types and macros */
+/* size_t is unsigned integral type and is the result of sizeof keyword */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
