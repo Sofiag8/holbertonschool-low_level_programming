c2012899f0d6b7c2ccba56b6dcb5b32aa6fb3ae1
Sofiag8 <1943@holbertonschool.com>
2020-07-15 17:21:54 +0000

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,10 @@
  * @name: pointer to a char
  * @f: a pointer to function that receives a pointer to char
  *
- * Return: void
+ * Return: void or NULL
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 		f(name);
 }
