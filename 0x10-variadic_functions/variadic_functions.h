b76393f9237a3282cff37b83cb7a580b7b37935f
Sofiag8 <1943@holbertonschool.com>
2020-07-18 18:40:05 +0000

    correction pointers of the typedef struc

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,25 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-
-typedef struct characters
-{
-        /* pointer to the arguments passed to the function print_all) */
-        char arguments;
-        /* pointer to function print_all that returns nothing */
-        /* and receive a list of characters(arguments) */
-	/* va_list is a type that iterates arguments */
-	/* va_list as type of  variable we're going to name charlist */
-	/* so the pointer points a function that receive va_list) */
-        void (ptrfunc)(va_list);
-} characters;
 
 void printchar(va_list charlist);
 void printinteger(va_list charlist);
 void printfloat(va_list charlist);
 void printstring(va_list charlist);
+
+typedef struct characters
+{
+        /* pointer to the arguments passed to the function print_all) */
+        char *arguments;
+        /* pointer to function print_all that returns nothing */
+        /* and receive a list of characters(arguments) */
+	/* va_list is a type that iterates arguments */
+	/* va_list as type of  variable we're going to name charlist */
+	/* so the pointer points a function that receive va_list) */
+        void (*ptrfunc)(va_list);
+} characters;
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
