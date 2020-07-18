bd072572034d77bd0a667aa021674dcac9ccd972
Sofiag8 <1943@holbertonschool.com>
2020-07-18 18:19:07 +0000

    add header now with typedef for task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct characters
+{
+        /* pointer to the arguments passed to the function print_all) */
+        char arguments;
+        /* pointer to function print_all that returns nothing */
+        /* and receive a list of characters(arguments) */
+	/* va_list is a type that iterates arguments */
+	/* va_list as type of  variable we're going to name charlist */
+	/* so the pointer points a function that receive va_list) */
+        void (ptrfunc)(va_list);
+} characters;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
