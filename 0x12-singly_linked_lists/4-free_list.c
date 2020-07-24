d496a86b3829b8c0c0ed0ea66bc7bccc85b07fca
Sofiag8 <1943@holbertonschool.com>
2020-07-24 03:20:47 +0000

    comments added to understanding

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,20 @@
 void free_list(list_t *head)
 {
 	list_t *firstnode;
-
+	/* check if head is null*/
+	/* if yes the list is empty and we just return */
 	while (head != NULL)
 	{
+		/* save the head in a firstnode varibale */
 		firstnode = head;
+		/* and make head point to the next node */
+		/* on the list_t */
 		head = head->next;
+		/* now safelyu free the reference to str */
 		free(firstnode->str);
+		/* and free safely firstnode */
 			free(firstnode);
+			/*remember we freed memory fron the most inside */
+			/* to outside */
 	}
 }
