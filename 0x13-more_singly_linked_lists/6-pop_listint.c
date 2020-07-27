43b81968239a9537ac2dff7270465f007f3ad342
Sofiag8 <1943@holbertonschool.com>
2020-07-27 23:31:36 +0000

    Pop, delete head node from the list 2

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,19 +8,19 @@
 int pop_listint(listint_t **head)
 {
 	int n = 0;
-	listint_t *temp;
-
+	listint_t *ptraux;
+	/* task condition */
 	if (head == NULL)
 		return (0);
 	/* n is equal to the head de reference to n from the structure */
 	/* that is the szie of the list */
 	n = (*head)->n;
 	/* now temp is the new head node */
-	temp = *head;
+	ptraux = *head;
 	/* head goes to the next node */
 	*head = (*head)->next;
 	/* frees temp so the head node is deleted */
-	free(temp);
+	free(ptraux);
 	/* return the head nodes data that is n according to the structure */
 	return (n);
 }
