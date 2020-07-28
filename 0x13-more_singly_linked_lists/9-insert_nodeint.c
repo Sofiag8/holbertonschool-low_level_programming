31fe0976c2b12f991127b9554c495f2d4914861a
Sofiag8 <1943@holbertonschool.com>
2020-07-28 07:10:40 +0000

    fixed and explained

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,23 +13,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int size;
 
 	size = 0;
+	/* allocate memory for the newnode */
 	newnode = malloc(sizeof(listint_t));
-
+	/* task condition */
 	if (newnode == NULL)
 		return (NULL);
+	/* n keeps being the data of the new node */
 	newnode->n = n;
+	/*  given the case, if index is cero then newnode is equal to head */
+	/* and return newnode in head of the list */
 	if (idx == 0)
 	{
 		newnode->next = *head;
 		*head = newnode;
 		return (newnode);
 	}
+	/* usea de auxiliar */
 	headaux = *head;
+	/* if index is different to the position */
+	/* counter size keeps running and headaux moves to the next node */
 	while (headaux != NULL && size != idx - 1)
 	{
 		size++;
 		headaux = headaux->next;
 	}
+	/* if index and size are equal and non NULL */
 	if (size == idx - 1 && headaux != NULL)
 	{
 		newnode->next = headaux->next;
