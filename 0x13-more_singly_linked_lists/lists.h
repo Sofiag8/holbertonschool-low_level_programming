17aeadafd0925b05a0c5b73b6d270fd0a2cffdc1
Sofiag8 <1943@holbertonschool.com>
2020-07-28 05:22:58 +0000

    header 10

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 void sumofnodes(listint_t *head, int *sum);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 #endif
