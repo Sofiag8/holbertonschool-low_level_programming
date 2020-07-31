f29bd982b7d8624e51321cb10f02246662fe41df
Sofiag8 <1943@holbertonschool.com>
2020-07-31 03:17:57 +0000

    5.101 3

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	/* Return count of set bits in */
 	/* n XOR m */
-	return (counsetbits(n ^ m));
+	return (countsetbits(n ^ m));
 }
 /**
  * countsetbits - count set bits
