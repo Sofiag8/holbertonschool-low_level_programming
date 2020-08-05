6ad8fb39e37d7ea567b69c82b552d5305f7e0f30
Sofiag8 <1943@holbertonschool.com>
2020-08-05 18:04:12 +0000

    header 8

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,7 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(char *file_from, char *file_to);
-void Betty(char *file_from);
-int main(int ac, char **av);
+int main(int argc, char **argv);
+
 #endif
