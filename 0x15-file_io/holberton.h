1912bf99a1bca7cfbfe668a400b3564c943f3b0e
Sofiag8 <1943@holbertonschool.com>
2020-08-05 05:03:04 +0000

    corrected

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,4 +1,4 @@
-B#ifndef _HOLBERTON_H
+#ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
 #include <stdio.h>
@@ -10,7 +10,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(char *file_from, char *file_to);
+void Betty(char *file_from);
 int main(int ac, char **av);
-void Betty(char *file_from)
-
 #endif
