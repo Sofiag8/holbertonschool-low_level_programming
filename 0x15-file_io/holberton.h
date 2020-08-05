138267b8499a74e53ccbc498c6dcdf18aa7e804e
Sofiag8 <1943@holbertonschool.com>
2020-08-05 02:38:24 +0000

    header with the last task

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
+#define SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,5 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(char file_to, char file_from);
 
 #endif
