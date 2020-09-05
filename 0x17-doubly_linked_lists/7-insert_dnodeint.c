f6b37fadd61fd943a9180c0ef8c9c35413b311e4
Sofiag8 <1943@holbertonschool.com>
2020-09-05 19:29:15 +0000

    fixing comparition to NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,11 +12,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *aux_h;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
 	/* case asked idx is 0 or h is null at the beggining, recursion */
-	if (idx == 0 || !(*h))
+	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
@@ -35,6 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	}
 	/* idx found then add new_node */
+	new_node->n = n;
 	new_node->prev = aux_h;
 	new_node->next = aux_h->next;
 	aux_h->next->prev = new_node;
