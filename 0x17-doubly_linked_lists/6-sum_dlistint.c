e7a669b5af657c040eeebdc0bcef754a23f9f016
Sofiag8 <1943@holbertonschool.com>
2020-09-05 19:09:44 +0000

    add list correctio aux variable

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -12,8 +12,8 @@ int sum_dlistint(dlistint_t *head)
 	/* traverse the list and add each node value */
 	while (head)
 	{
-		add_data += head->n;
-		head = head->next;
+		add_data += aux_head->n;
+		aux_head = aux_head->next;
 	}
 	return (add_data);
 }
