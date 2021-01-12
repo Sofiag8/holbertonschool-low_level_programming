dbd881bf2604c4e7fa18f29d267a3118e8e9f34f
Sofiag8 <1943@holbertonschool.com>
2021-01-12 22:26:47 +0000

    header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCHALGOS_H
+#define _SEARCHALGOS_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+#endif
